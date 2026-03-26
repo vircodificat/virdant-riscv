3c11c9921f5e8a8a2a667b051af5213403236032
Vir Codificat <vircodificat@gmail.com>
2026-03-26 16:54:35 -0700

    More progress.

--- loop.s
+++ loop.s
@@ -2,10 +2,15 @@
 .globl _start
 
 _start:
-    li t0, 10        # loop counter
+    li t0, 4
+    li t1, 0
 
 loop:
     addi t0, t0, -1 # decrement counter
     bnez t0, loop   # if t0 != 0, continue looping
+
+    addi t1, t1, 1
+    li t0, 4
+    jal loop
 
     ebreak          # stop execution / trigger debugger
--- sim.cpp
+++ sim.cpp
@@ -3,6 +3,8 @@
 #include "verilated_vcd_c.h"
 
 vluint64_t main_time = 0;
+vluint64_t clock_period_ns = 24;
+vluint64_t half_clock_period_ps = clock_period_ns * 1000 / 2;
 
 double sc_time_stamp() {
     return main_time;
@@ -12,12 +14,12 @@ void tick(VTop *top, VerilatedVcdC* tfp) {
     top->clock = 1;
     top->eval();
     tfp->dump(main_time);
-    main_time += 5;
+    main_time += half_clock_period_ps;
 
     top->clock = 0;
     top->eval();
     tfp->dump(main_time);
-    main_time += 5;
+    main_time += half_clock_period_ps;
 }
 
 void reset(VTop *top, VerilatedVcdC* tfp) {
